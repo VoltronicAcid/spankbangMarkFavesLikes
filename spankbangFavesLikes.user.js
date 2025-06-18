6d51dbd0a5c703281e7d6aa9ada7477fa98e20e7
VoltronicAcid <78665774+VoltronicAcid@users.noreply.github.com>
2025-06-17 21:39:19 -0400

    metadata

---
+++ spankbangFavesLikes.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name          SpankBang - Mark Faves and Likes
+// @description   Highlights the liked and favorite buttons on videos
+// @author        VoltronicAcid
+// @version       0.0.1
+// @homepageURL   https://github.com/VoltronicAcid/spankbangMarkFavesLikes
+// @supportURL    https://github.com/VoltronicAcid/spankbangMarkFavesLikes/issues
+// @match         http*://*.spankbang.com/*/playlist/*
+// @match         http*://*.spankbang.com/*/video/*
+// @run-at        document-idle
+// ==/UserScript==
