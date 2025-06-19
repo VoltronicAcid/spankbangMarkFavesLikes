1e42c2299dfc290915194e75b4505507f630127b
VoltronicAcid <78665774+VoltronicAcid@users.noreply.github.com>
2025-06-19 08:32:42 -0400

    populate likes

--- spankbangFavesLikes.user.js
+++ spankbangFavesLikes.user.js
@@ -2,20 +2,23 @@
 // @name          SpankBang - Mark Faves and Likes
 // @description   Highlights the liked and favorite buttons on videos
 // @author        VoltronicAcid
-// @version       0.0.1
+// @version       0.0.3
 // @homepageURL   https://github.com/VoltronicAcid/spankbangMarkFavesLikes
 // @supportURL    https://github.com/VoltronicAcid/spankbangMarkFavesLikes/issues
-// @match         http*://*.spankbang.com/*/playlist/*
+// @match         http*://*.spankbang.com/*-*/playlist/*
 // @match         http*://*.spankbang.com/*/video/*
 // @run-at        document-idle
 // ==/UserScript==
 
 const VID_ID = document.getElementById("video")?.dataset.videoid;
+const FAVE_STORE = "favedVideos";
+const LIKE_STORE = "likedVideos";
 
 const logMessage = (msg) => {
-    const logStyle = "background-color:cornsilk; color:darkblue; font-size: 12pt; padding: 5px";
+    const logStyle = "background-color:aliceblue; color:darkblue; font-size: 12pt; padding: 5px";
     console.log(`%c${msg}`, logStyle);
 };
+
 const logError = (msg) => {
     const logStyle = "background-color:crimson; color:ivory; font-size: 12pt; padding: 5px";
     console.log(`%c${msg}`, logStyle);
@@ -29,10 +32,10 @@ const getDatabase = async () => {
             logMessage("Upgrading/Installing database");
             const { result: db } = openRequest;
 
-            const favesStore = db.createObjectStore("favedVideos", { keyPath: "id" });
+            const favesStore = db.createObjectStore(FAVE_STORE, { keyPath: "id" });
             favesStore.createIndex("faveIndex", "id", { unique: true });
 
-            const likesStore = db.createObjectStore("likedVideos", { keyPath: "id" });
+            const likesStore = db.createObjectStore(LIKE_STORE, { keyPath: "id" });
             likesStore.createIndex("likeIndex", "id", { unique: true });
         };
 
@@ -51,10 +54,8 @@ const getDatabase = async () => {
         openRequest.onerror = function () {
             logError(`Error with open database request.`);
             const { code, message, name } = openRequest.error;
-            console.error(name, code);
-            console.error(message)
-            // const { result: db } = openRequest;
-            // db.close();
+            console.error(code, name);
+            console.error(message);
             reject(message);
         }
     });
@@ -62,58 +63,83 @@ const getDatabase = async () => {
     return databasePromise;
 };
 
-const getPage = async (url) => {
-    logMessage(`Requesting page: ${url}`);
-    const response = await fetch(url);
-    if (response.ok) {
-        const parser = new DOMParser();
-        const respText = await response.text();
-        return parser.parseFromString(respText, "text/html");
+async function* getPages(url) {
+    while (url) {
+        try {
+            logMessage(`Fetching URL:\t${url}`);
+            const response = await fetch(url);
+            if (response.ok) {
+                const html = await response.text();
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(html, "text/html");
+                url = doc.querySelector("li.next > a").href;
+                yield doc;
+            } else {
+                throw new Error(`Unable to fetch URL:\n${url}\nStatus Code:\t${response.status}`);
+            }
+        } catch (err) {
+            console.error(err);
+            console.trace(err);
+            break;
+        }
     }
+};
 
-    throw new Error("Unable to get URL");
-}
+const getFavoritesHref = async () => {
+    try {
+        const response = await fetch(`${document.location.origin}/users/playlists`);
+        if (response.ok) {
+            const html = await response.text();
+            const parser = new DOMParser();
+            const page = parser.parseFromString(html, "text/html");
+
+            return page.querySelector('a.playlist-item[href$="/favorites/"]').href;
+        }
+    } catch (err) {
+        console.error(err);
+        console.trace(err);
+    }
+};
 
 const getAllFavorites = async () => {
-    const userPlaylistsURL = `${document.location.origin}/users/playlists`;
-    const userPlaylistsPage = await getPage(userPlaylistsURL);
-    const favoritesLink = userPlaylistsPage.querySelector('a.playlist-item[href$="/favorites/"]');
-
-    let favoritesPage = await getPage(favoritesLink.href);
-    let nextlink = null;
     let videos = [];
 
-    do {
+    for await (const page of getPages(await getFavoritesHref())) {
         videos = videos.concat(
             Array.from(
-                favoritesPage.getElementsByClassName("video-item"))
-                .map(div => ({ id: div.dataset.id, title: div.querySelector("a.thumb").title })
+                page.getElementsByClassName("video-item"))
+                .map((div) => ({ id: div.dataset.id, title: div.querySelector("a.thumb").title })
                 )
         );
-
-        nextLink = favoritesPage.querySelector("li.next");
-        const nextURL = nextLink.querySelector("a").href;
-
-        if (nextURL) favoritesPage = await getPage(nextLink.querySelector("a").href);
-    } while (!nextLink?.classList.contains("disabled"));
+    }
 
     return videos;
 };
 
-const getAllLikes = async () => { };
+const getAllLikes = async () => {
+    let videos = [];
+
+    for await (const page of getPages(`${document.location.origin}/users/liked`)) {
+        videos = videos.concat(
+            Array.from(
+                page.getElementsByClassName("video-item"))
+                .map((div) => ({ id: div.dataset.id, title: div.querySelector("a.thumb").title })
+                )
+        );
+    }
+
+    return videos;
+};
 
 const checkStorePopulated = async (db, storeName) => {
     const query = new Promise((resolve, reject) => {
         logMessage(`Checking if ${storeName} is populated.`);
         const transaction = db.transaction(storeName, "readonly");
-        // console.log(transaction);
         const store = transaction.objectStore(storeName);
-        // console.log(store);
 
         const request = store.getAllKeys();
         request.onsuccess = function () {
             const { result: keys } = request;
-            // console.log(keys);
             resolve(keys.length > 0);
         };
         request.onerror = function () {
@@ -128,15 +154,15 @@ const checkStorePopulated = async (db, storeName) => {
 const highlightFaveIcon = () => {
     logMessage("Coloring Fave");
     const heart = document.querySelector("div.fv > svg.i_svg.i_new-ui-heart-outlined");
-    heart.style.fill = "#f08e84";
+    if (heart) heart.style.fill = "#f08e84";
 };
 
 const populateFavesStore = async (db) => {
     logMessage("Populating favorites");
     const vids = await getAllFavorites();
-    // console.log(vids);
+
     const insert = new Promise((resolve, reject) => {
-        const transaction = db.transaction("favedVideos", "readwrite");
+        const transaction = db.transaction(FAVE_STORE, "readwrite");
         transaction.oncomplete = function () {
             logMessage("Populate Faves Store transaction completed.");
             resolve();
@@ -147,7 +173,7 @@ const populateFavesStore = async (db) => {
             console.log(transaction);
             reject();
         };
-        const store = transaction.objectStore("favedVideos");
+        const store = transaction.objectStore(FAVE_STORE);
         for (const vid of vids) {
             store.add(vid);
             if (vid.id === VID_ID) {
@@ -161,37 +187,57 @@ const populateFavesStore = async (db) => {
 
 const highlightLikeIcon = () => {
     logMessage("Coloring Like");
+    const checkmark = document.querySelector("span.hot > svg.i_svg.i_new-ui-checkmark-circle-outlined");
+    if (checkmark) {
+        checkmark.style.fill = "dodgerblue";
+        return;
+    }
+
     const thumbsUp = document.querySelector("span.hot > svg.i_svg.i_new-ui-thumbs-up");
-    thumbsUp.style.fill = "dodgerblue";
+    if (thumbsUp) thumbsUp.style.fill = "dodgerblue";
 };
 
 const populateLikesStore = async (db) => {
     logMessage("Populating likes");
-};
+    const vids = await getAllLikes();
 
-const getFaveLikeStatus = async (videoid) => {
-    return { isFave: false, isLike: true };
-}
+    const insert = new Promise((resolve, reject) => {
+        const transaction = db.transaction(LIKE_STORE, "readwrite");
+        transaction.oncomplete = function () {
+            logMessage("Populate Likes Store transaction completed.");
+            resolve();
+        };
+        transaction.onerror = function (event) {
+            logError("Populate Likes Store transaction generated an error.");
+            console.log(event);
+            console.log(transaction);
+            reject();
+        };
+        const store = transaction.objectStore(LIKE_STORE);
+        for (const vid of vids) {
+            store.add(vid);
 
-const highlightIcons = (status) => {
-    if (status.isFave) highlightFaveIcon();
-    if (status.isLike) highlightLikeIcon();
+            if (vid.id === VID_ID) highlightLikeIcon();
+        }
+    });
+
+    return insert;
 };
 
 const main = async () => {
     logMessage("Faves/Likes script is running");
-    logMessage(`Video ID:\t${VID_ID}`)
+    logMessage(`Video ID:\t${VID_ID}`);
+
     try {
         const db = await getDatabase();
-        // console.log(db);
-        const favesPopulated = await checkStorePopulated(db, "favedVideos");
+        const favesPopulated = await checkStorePopulated(db, FAVE_STORE);
         logMessage(`Faves store populated:\t${favesPopulated}`);
 
         if (!favesPopulated) {
             populateFavesStore(db);
         }
 
-        const likesPopulated = await checkStorePopulated(db, "likedVideos");
+        const likesPopulated = await checkStorePopulated(db, LIKE_STORE);
         logMessage(`Likes store populated:\t${likesPopulated}`);
 
         if (!likesPopulated) {
@@ -201,8 +247,6 @@ const main = async () => {
         console.trace(err);
         return;
     }
-
-    getFaveLikeStatus(VID_ID).then(highlightIcons);
 };
 
 main();
